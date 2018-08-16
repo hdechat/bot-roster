8a39b27d815be166de62d25def13db40810ff976
Helen Dechat <hdechat@gmail.com>
2018-08-15 22:57:13 -0600

    Add tests for App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper = shallow(<App />);
+  let mockTeam = {
+    teamName: 'The Zipperheads',
+    starters: [{robot: 1}],
+    subs: [{robot: 2}],
+    error: ''
+  };
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('adds team to team array in state', () => {
+    wrapper.instance().addTeamRoster(mockTeam);
+
+    const actual = wrapper.state();
+    const expected = { teams: [mockTeam]};
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('renders all expected elements', () => {
+    expect(wrapper.find('div.App').length).toEqual(1);
+    expect(wrapper.find('header').length).toEqual(1);
+    expect(wrapper.find('h1').length).toEqual(1);
+    expect(wrapper.find('GenerateTeamRoster').length).toEqual(1);
+  });
 });
