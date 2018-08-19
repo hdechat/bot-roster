826ad29ac6a278cc389656e49e75d5888fc67647
Helen Dechat <hdechat@gmail.com>
2018-08-18 20:36:07 -0600

    Add mock data with sample team roster

---
+++ src/mock_data/sample-team.js
@@ -0,0 +1,158 @@
+module.exports = {
+  teamName: 'Buzz Killers',
+  subs: [
+    {
+      id: 'RoJe01',
+      firstName: 'Rosie',
+      lastName: 'Jetson',
+      speed: 1,
+      strength: 2,
+      agility: 3,
+      totalAttrScore: 6,
+      category: 'subs'
+    },
+    {
+      id: 'LiVo02',
+      firstName: 'Lion Force',
+      lastName: 'Voltron',
+      speed: 2,
+      strength: 3,
+      agility: 4,
+      totalAttrScore: 9,
+      category: 'subs'
+    },
+    {
+      id: 'MeGo03',
+      firstName: 'Mecha',
+      lastName: 'Godzilla',
+      speed: 3,
+      strength: 4,
+      agility: 5,
+      totalAttrScore: 12,
+      category: 'subs'
+    },
+    {
+      id: 'ED2004',
+      firstName: 'ED',
+      lastName: '209',
+      speed: 4,
+      strength: 5,
+      agility: 6,
+      totalAttrScore: 15,
+      category: 'subs'
+    },
+    {
+      id: 'LiMe05',
+      firstName: 'Liquid',
+      lastName: 'Metal',
+      speed: 5,
+      strength: 6,
+      agility: 7,
+      totalAttrScore: 18,
+      category: 'subs'
+    }
+  ],
+  starters: [
+    {
+      id: 'IrGi10',
+      firstName: 'Iron',
+      lastName: 'Giant',
+      speed: 10,
+      strength: 11,
+      agility: 12,
+      totalAttrScore: 33,
+      category: 'starters'
+    },
+    {
+      id: 'JoFi11',
+      firstName: 'Johnny',
+      lastName: 'Five',
+      speed: 11,
+      strength: 12,
+      agility: 13,
+      totalAttrScore: 36,
+      category: 'starters'
+    },
+    {
+      id: 'RoRo12',
+      firstName: 'Robby',
+      lastName: 'Robot',
+      speed: 12,
+      strength: 13,
+      agility: 14,
+      totalAttrScore: 39,
+      category: 'starters'
+    },
+    {
+      id: 'AsBo13',
+      firstName: 'Astro',
+      lastName: 'Boy',
+      speed: 13,
+      strength: 14,
+      agility: 15,
+      totalAttrScore: 42,
+      category: 'starters'
+    },
+    {
+      id: 'MoKu14',
+      firstName: 'Motoko',
+      lastName: 'Kusanagi',
+      speed: 14,
+      strength: 15,
+      agility: 16,
+      totalAttrScore: 45,
+      category: 'starters'
+    },
+    {
+      id: 'Bi3415',
+      firstName: 'Bishop',
+      lastName: '341-B',
+      speed: 15,
+      strength: 16,
+      agility: 17,
+      totalAttrScore: 48,
+      category: 'starters'
+    },
+    {
+      id: 'MeMa16',
+      firstName: 'Mega',
+      lastName: 'Man',
+      speed: 16,
+      strength: 17,
+      agility: 18,
+      totalAttrScore: 51,
+      category: 'starters'
+    },
+    {
+      id: 'Ha9017',
+      firstName: 'Hal',
+      lastName: '9000',
+      speed: 17,
+      strength: 18,
+      agility: 19,
+      totalAttrScore: 54,
+      category: 'starters'
+    },
+    {
+      id: 'BeRo18',
+      firstName: 'Bender',
+      lastName: 'Rodriguez',
+      speed: 18,
+      strength: 19,
+      agility: 20,
+      totalAttrScore: 57,
+      category: 'starters'
+    },
+    {
+      id: 'OpPr19',
+      firstName: 'Optimus',
+      lastName: 'Prime',
+      speed: 19,
+      strength: 20,
+      agility: 21,
+      totalAttrScore: 60,
+      category: 'starters'
+    }
+  ],
+  error: ''
+}
